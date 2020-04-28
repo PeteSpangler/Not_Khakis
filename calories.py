aa029624e8bbfb1dd679cc377b467eeb384d6b9a
Pete Spangler <63021506+PeteSpangler@users.noreply.github.com>
2020-04-28 09:17:39 -0700

    created simple calorie program

---
+++ calories.py
@@ -0,0 +1,11 @@
+print("How many calories do you consume in a day?")
+breakfast_calories = int(input("Breakfast calories?: "))
+lunch_calories = int(input("Lunch calories?: "))
+snack_calories = int(input("Snack calories?: "))
+dinner_calories = int(input("Dinner calories: "))
+total_calories = breakfast_calories + lunch_calories + snack_calories + dinner_calories
+print("Your total calories for today: ", total_calories)
+
+
+
+
