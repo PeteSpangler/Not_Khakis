26fdfbbc4d7976a25bc89ce4df4d919db75e67d8
Pete Spangler <63021506+PeteSpangler@users.noreply.github.com>
2020-04-29 13:22:47 -0700

    Y

--- arrayproblem.py
+++ arrayproblem.py
@@ -1,7 +1,7 @@
 #unsorted int array, all valid values between 1 and n
 # where n is size of an array
 # find duplicates in the array and print them to the screen
-int_array = [1, 1, 2, 15, 12, 8, 2, 4, 10, 11, 32, 23, 9, 3, 6, 4, 88, 27, 54] #random int array I came up with?
+int_array = [1, 1, 2, 15, 12, 8, 2, 88, 6, 32, 23, 4, 10, 11, 32, 23, 9, 3, 6, 4, 88, 27, 54] #random int array I came up with?
 #check if there are duplicates in list by comparing length of list vs length of list as set
 def dupes(int_array):
     if len(int_array) == len(set(int_array)):
