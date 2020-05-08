c33fcb34704c5417875490b1d7d683fd4ba01e96
Pete Spangler <63021506+PeteSpangler@users.noreply.github.com>
2020-05-08 08:24:03 -0700

    First Data Structure HW

---
+++ Homeworks/Homework2_DataStructurept1.py
@@ -0,0 +1,17 @@
+# What I want you to work on from here to Sunday is learn how to create basic data structures in Python. 
+# I want you to use classes, and start with the following:
+
+# 1) A class called IntNode, which holds an int value and a pointer to another IntNode.
+
+# 2) A class called IntList, which has two pointers to IntNodes, one called head, 
+# and another one called tail. Also, I want IntList to have the following methods:
+# Size(), which returns an int with the amount of nodes in the list.
+# Insert(int value) which inserts the value at the end of the list.
+# Contains(int value) which returns true or false depending on whether the value is contained in the list or not.
+# Delete(int position) which deletes the value at the specified position.
+# Get(int position) which returns the int value at the specified position.
+# 3) Finally, a small program that inserts values 5,3,6 and 5 into your list, 
+# and then iterates with a loop to extract the values and print them on screen.
+
+# After you're done with this, let me know and send me a link to the python file(s) 
+# in GitHub so I can take a look and then we can talk about small algorithms and more complex data structures to play with.
