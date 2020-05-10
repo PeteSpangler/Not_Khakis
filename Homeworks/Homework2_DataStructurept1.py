6537e1f1d8713b966e1432ccd556268127721e30
Pete Spangler <63021506+PeteSpangler@users.noreply.github.com>
2020-05-10 13:29:09 -0700

    got lucky

--- Homeworks/Homework2_DataStructurept1.py
+++ Homeworks/Homework2_DataStructurept1.py
@@ -9,6 +9,15 @@ class IntNode:
         self.value = value
         self.next = None
 
+    def get_value(self):
+        return self.value
+
+    def get_next(self):
+        return self.next
+
+    def set_next(self, new_next):
+        self.next = new_next
+
 # 2) A class called IntList, which has two pointers to IntNodes,
 # one called head, and another one called tail. 
 # Also, I want IntList to have the following methods:
@@ -26,38 +35,37 @@ class IntList:
     def Size(self):
         current = self.head
         count = 0
-        while current:
+        while current != None:
             count += 1
             current = current.get_next()
         return count
     
-    def insert(self, new_data):
-        new_IntNode = IntNode(new_data)
-        if self.head is None:
-            self.head = new_IntNode
-            return
-        last = self.head
-        while (last.next):
-            last = last.next
-        last.next = new_IntNode
+    def append(self, value):
+        new_node = IntNode(value)
+        if self.tail == None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.set_next(new_node)
+            self.tail = new_node
 
     def Contains(self, x):
         current = self.head
         while current != None:
-            if current.data ==x:
+            if current.value == x:
                 return True
             current = current.next
         return False
 
-    def Delete(self, position):
-        if self.head ==None:
+    def Delete(self, value):
+        if self.head == None:
             return
         temp = self.head
-        if position == 0:
+        if value == 0:
             self.head = temp.next
             temp = None
             return
-        for i in range(position - 1):
+        for i in range(value - 1):
             temp = temp.next
             if temp is None:
                 break
@@ -68,11 +76,23 @@ class IntList:
         next = temp.next.next
         temp.next = None
         temp.next = next
+#do something about tail
 
+    def print(self):
+        temp = self.head
+        while temp != None:
+            print(temp.get_value())
+            temp = temp.get_next()
 
+numbers = IntList()
+numbers.append(1)
+numbers.append(3)
+numbers.append(5)
+numbers.print()
+numbers.Delete(2)
+numbers.print()
 # 3) Finally, a small program that inserts values 5,3,6 and 5 into your list, 
-# and then iterates with a loop to extract the values and print them on screen.
-IntList.insert(1,2)
+# and then iterates with a loop to extract the values and print them on screen
 # After you're done with this, let me know and send me a link to the python file(s) 
 # in GitHub so I can take a look and then we can talk about small algorithms and
 #  more complex data structures to play with.
